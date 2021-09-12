8fb19c36828149537d0b793ec8f2a6939c50bdbb
lawsuit3310 <aimeet5480@gmail.com>
2021-09-12 19:06:28 +0900

    Ex011과 Ex012 추가

--- Program.cs
+++ Program.cs
@@ -11,16 +11,20 @@ namespace RoadBook.CsharpBasic.Chapter01
         static void Main(string[] args)
         {
             //Examples.Ex006 ex6 = new Examples.Ex006();
-            Examples.Ex007 ex7 = new Examples.Ex007();
-            Examples.Ex008 ex8 = new Examples.Ex008();
-            Examples.Ex009 ex9 = new Examples.Ex009();
-            Examples.Ex010 ex10 = new Examples.Ex010();
+            //Examples.Ex007 ex7 = new Examples.Ex007();
+            //Examples.Ex008 ex8 = new Examples.Ex008();
+            //Examples.Ex009 ex9 = new Examples.Ex009();
+            //Examples.Ex010 ex10 = new Examples.Ex010();
+            Examples.Ex11_.Ex011 ex11 = new Examples.Ex11_.Ex011();
+            Examples.Ex11_.Ex012 ex12 = new Examples.Ex11_.Ex012();
 
             //ex6.Run();
-            ex7.Run();
-            ex8.Run(); //ex7의 코드에 명시적 형변환 추가
-            ex9.Run();
-            ex10.Run();
+            //ex7.Run();
+            //ex8.Run(); //ex7의 코드에 명시적 형변환 추가
+            //ex9.Run();
+            //ex10.Run();
+            ex11.Run();
+            ex12.Run();
         }
     }
 }
