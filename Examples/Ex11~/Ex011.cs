3118caf2f7aa60e774a6610bbc0323e9a09f07c7
lawsuit3310 <aimeet5480@gmail.com>
2021-09-12 19:07:31 +0900

    Ex011과 Ex012 추가

---
+++ Examples/Ex11~/Ex011.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadBook.CsharpBasic.Chapter01.Examples.Ex11_
+{
+    class Ex011
+    {
+        int globalValue = 20;
+
+        public void Run()
+        {
+            int localValue = 10;
+
+            Sum();
+            Multiple();
+
+            Console.WriteLine("global : {0} / local : {1}",globalValue,localValue);
+        }
+
+        private void Sum()
+        {
+            globalValue += 10;
+        }
+
+        private void Multiple()
+        {
+            globalValue *= 2;
+        }
+    }
+}
